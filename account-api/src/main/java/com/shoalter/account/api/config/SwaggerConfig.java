810b0bfe4ecc6263fa8c0734d20a62c5d65ea3fe
TW - IT - BE - Howard Wang <howard.wang@shoalter.com>
2023-03-13 16:53:53 +0800

    doc: add Open API

---
+++ account-api/src/main/java/com/shoalter/account/api/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.shoalter.account.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Demo Account API"))
+public class SwaggerConfig {
+}

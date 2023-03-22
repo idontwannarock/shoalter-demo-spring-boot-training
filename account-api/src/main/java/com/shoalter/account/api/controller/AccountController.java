1a943e90f1e1e94c1dc21235823f061b7b3edc70
TW - IT - BE - Howard Wang <howard.wang@shoalter.com>
2023-03-22 17:14:57 +0800

    feat: add access log at the beginning of each endpoint

--- account-api/src/main/java/com/shoalter/account/api/controller/AccountController.java
+++ account-api/src/main/java/com/shoalter/account/api/controller/AccountController.java
@@ -4,11 +4,13 @@ import com.shoalter.account.api.config.security.pojo.AuthenticatedUser;
 import com.shoalter.account.api.controller.dto.response.Response;
 import com.shoalter.account.api.usecase.ClockingUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("api/accounts")
 @RestController
@@ -18,6 +20,7 @@ public class AccountController {
 
 	@PostMapping("clocking")
 	public Response<Void> clocking(@AuthenticationPrincipal AuthenticatedUser user) {
+		log.info("{} clocking", user.getUserId());
 		clockingUseCase.start(user.getUserId());
 		return Response.success(null);
 	}
--- account-api/src/main/java/com/shoalter/account/api/controller/AuthController.java
+++ account-api/src/main/java/com/shoalter/account/api/controller/AuthController.java
@@ -13,9 +13,11 @@ import com.shoalter.account.api.usecase.LoginUseCase;
 import com.shoalter.account.api.usecase.RefreshAccessTokenUseCase;
 import com.shoalter.account.api.usecase.RegisterUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("api/auth")
 @RestController
@@ -28,16 +30,19 @@ public class AuthController {
 
 	@PostMapping("registration")
 	public Response<RegisterResponse> register(@RequestBody RegisterRequest request) {
+		log.info("{} registering", request.getUsername());
 		return Response.success(registerUseCase.start(request.getUsername(), request.getPassword()));
 	}
 
 	@PostMapping("login")
 	public Response<LoginResponse> login(@RequestBody LoginRequest request) {
+		log.info("{} login", request.getUsername());
 		return Response.success(loginUseCase.start(request.getUsername(), request.getPassword()));
 	}
 
 	@PostMapping("access-token")
 	public Response<RefreshAccessTokenResponse> refreshAccessToken(@RequestBody RefreshAccessTokenRequest request) {
+		log.info("refreshing access token with refresh token {}", request.getRefreshToken());
 		return Response.success(refreshAccessTokenUseCase.start(request.getRefreshToken()));
 	}
 
@@ -45,6 +50,7 @@ public class AuthController {
 	public Response<Void> changePassword(
 			@PathVariable Integer accountId,
 			@Validated @RequestBody ChangePasswordRequest request) {
+		log.info("{} changing password", accountId);
 		changePasswordUseCase.start(accountId, request);
 		return Response.success(null);
 	}
